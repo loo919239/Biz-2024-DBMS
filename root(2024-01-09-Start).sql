e0d514a199bb8d6ee2cc9f9b2655b7a152752a61
loo919239 <loo919239@naver.com>
2024-01-10 13:39:23 +0900

    DBMS

---
+++ root(2024-01-09-Start).sql
@@ -0,0 +1,16 @@
+/* 
+여러줄 주석문
+ */
+-- 한줄짜리 주석
+/*
+이 화면에서 SQL 명령어를 입력하고
+ctrl + Enter 를 입력하면 명령이 실행된다
+명령어는 대체로 대소문자 구분이 없다
+중요 키워드는 대문자로, 변수 등은 소문자로 입력
+*/
+
+SHOW DATABASES;
+SHOW DATABASES;
+USE mysql;
+SHOW TABLES;
+SELECT % FROM user;
